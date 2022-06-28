695269d144b87893d31e884d1ddbe0c55dadd0ed
Sephens <ombamstephodhis@gmail.com>
2022-06-28 19:05:48 +0300

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+
+    # Python abs() function is used to return the absolute value of a number    # , i.e., it will remove the negative sign of the number.
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
