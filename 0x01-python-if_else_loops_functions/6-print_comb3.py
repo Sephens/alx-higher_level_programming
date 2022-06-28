e69cdc769f90224e065d3813de77a001473976cb
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-28 17:59:17 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for n in range(0, 8):
     for k in range(n + 1, 10):
-        print("{:d}{:d}".format(n, k),end=', ')
+        print("{:d}{:d}".format(n, k), end=', ')
 print("{:d}{:d}".format(n + 1, k))
-
