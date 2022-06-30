4e09473135cbb1c832ea18933dfa0981a26a649b
Sephens <ombamstephodhis@gmail.com>
2022-06-30 21:50:09 +0300

    executable

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,4 @@
 #!/usr/bin/python3
-# Write a program that imports all functions from the file calculator_1.py and handles basic operations.
-
-# Usage: ./100-my_calculator.py a operator b
-# If the number of arguments is not 3, your program has to:
-# print Usage: ./100-my_calculator.py <a> <operator> <b> followed with a new line
-# exit with the value 1
-# operator can be:
-# + for addition
-# - for subtraction
-# * for multiplication
-# / for division
-# If the operator is not one of the above:
-# print Unknown operator. Available operators: +, -, * and / followed with a new line
-# exit with the value 1
-# You can cast a and b into integers by using int() (you can assume that all arguments will be castable into integers)
-# The result should be printed like this: <a> <operator> <b> = <result>, followed by a new line
-# You are not allowed to use * for importing or __import__
-# Your code should not be executed when imported
 
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
