4ef2ee0b3e555b18c4ae7b9606ef69f785e5f67c
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-30 21:52:59 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-# Write a program that prints all the names defined by the compiled module hidden_4.pyc (please download it locally).
-
-# You should print one name per line, in alpha order
-# You should print only names that do not start with __
-# Your code should not be executed when imported
-# Make sure you are running your code in Python3.8.x (hidden_4.pyc has been compiled with this version)
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
