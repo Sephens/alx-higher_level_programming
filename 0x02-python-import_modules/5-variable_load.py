786d65e61c99486d9d7a52ed810ede68ab8b76e4
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-30 21:42:23 +0300

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# 5-variable_load.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Write a program that imports the variable a from the file variable_load_5.py and prints its value.
+
+# You are not allowed to use * for importing or __import__
+# Your code should not be executed when imported
 
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
