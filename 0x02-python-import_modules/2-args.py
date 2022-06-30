402827b3a7835a90d089f6c32d868309cb771d1c
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-30 20:59:47 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,10 +12,17 @@
 # followed by th# e argument value and a new line
 # Your code should not be executed when imported
 # The number of elements of argv can be retrieved by using: len(argv)
+
 if __name__ == "__main__":
-    from sys import argv
-    length = len(argv)
-    print("{:d} {:s}{:s}".format(length-1,"argument", if length <=2 else "arguments", ".", if length ==  1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
