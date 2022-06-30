6d3ad563276c9e95d36f85fabc76fc0a0e26222f
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-30 21:54:33 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-# Write a program that prints the number of and the list of its arguments.
-# The output should be:
-# Number of argument(s) followed by
-# argument (if number is one) or arguments# (otherwise),
-# followed by
-# : (or . if no arguments were passed) followed by
-# a new line, 
-# followed by (if at least one argument),
-# one line per argument:
-# the position of the argument (starting at 1) followed by :,
-# followed by th# e argument value and a new line
-# Your code should not be executed when imported
-# The number of elements of argv can be retrieved by using: len(argv)
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
