3e94171f142d430a0921e5ecfe1d3b41412dfd2a
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-07-06 21:24:25 +0300

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 7-update_dictionary.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Write a function that replaces or adds key/value in a dictionary.
 
 
 def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
+    # Replace or add key/value pairs in a dictionary.
     a_dictionary[key] = value
     return (a_dictionary)
