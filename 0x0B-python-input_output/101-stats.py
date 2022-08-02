1ba570ecc9c9611692fb71e7113f3e0a8b55aad2
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-08-03 00:37:06 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
-"""
-
-
 def print_stats(size, status_codes):
     """Print accumulated metrics.
 
