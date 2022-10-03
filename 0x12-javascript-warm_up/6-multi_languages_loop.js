1e24f80e7af8963471cebad0bf3d0f0662b9087a
Sephens <adesephens@gmail.com>
2022-10-03 22:45:56 +0300

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-// The first line: “C is fun”
-// The second line: “Python is cool”
-// The third line: “JavaScript is amazing”
-// You must use console.log(...) to print all output
-// You are not allowed to use var
-// You are not allowed to use any if/else statement
-// You can use only one console.log
-// You must use a loop (while, for, etc.)
-
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
